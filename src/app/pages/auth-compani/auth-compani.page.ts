0b8937cba5fc0845a8bd3539df108561fba3f14c
GabrielCarrilloF <gabocarrillo.deve@gmail.com>
2024-11-24 03:52:09 -0500

    SCRUM-33 <fron regsitro>

--- src/app/pages/auth-compani/auth-compani.page.ts
+++ src/app/pages/auth-compani/auth-compani.page.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-auth-compani',
   templateUrl: './auth-compani.page.html',
   styleUrls: ['./auth-compani.page.scss'],
 })
-export class AuthCompaniPage implements OnInit {
+export class AuthCompaniPage {
+  currentStep = 1; // Controla la sección visible
 
-  constructor() { }
+  formData = {
+    plan: '',
+    companyName: '',
+    logoUrl: '',
+    email: '',
+    phoneNumber: '',
+    address: '',
+    contactPerson: '',
+    username: '',
+    password: '',
+    confirmPassword: '',
+  };
 
-  ngOnInit() {
+  showPassword = false;
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
   }
 
+  nextStep() {
+    this.currentStep++;
+  }
+
+  previousStep() {
+    this.currentStep--;
+  }
+
+  register() {
+    if (this.formData.password !== this.formData.confirmPassword) {
+      alert('Passwords do not match!');
+      return;
+    }
+    console.log('Registration Data:', this.formData);
+  }
 }
