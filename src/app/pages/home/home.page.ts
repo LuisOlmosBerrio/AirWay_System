6280729479915117eafc9cb2980662ecf666a6aa
jesusbrave <jvaliente737@gmial.com>
2024-11-23 22:59:49 -0500

    avance front de vuelos

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, ViewChild, TemplateRef } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +7,32 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  
+  selectedDate: string;
 
-  constructor() {}
-  
+  @ViewChild('datePopoverTemplate', { static: true }) datePopoverTemplate!: TemplateRef<any>; // Usando el operador de aserción no nula
+
+  constructor(private popoverController: PopoverController) {
+    this.selectedDate = ''; // Inicializa la fecha seleccionada
+  }
+
+  async openDatePopover() {
+    const popover = await this.popoverController.create({
+      component: this.datePopoverTemplate,
+      translucent: true,
+      // Cambia esto para usar un template en lugar de un componente
+      // Puedes usar createEmbeddedView si necesitas un TemplateRef
+    });
+    
+    await popover.present();
+    
+    const { data } = await popover.onDidDismiss();
+    
+    if (data) {
+      this.selectedDate = data; // Actualiza la fecha seleccionada
+    }
+  }
+
+  closePopover() {
+    this.popoverController.dismiss(this.selectedDate); // Cierra el popover y pasa la fecha seleccionada
+  }
 }
