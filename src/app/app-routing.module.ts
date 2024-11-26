338820861e3e27f79634263de6ffc862b60dfa06
Luis Fernando Olmos Berrio <luisfernandoolmosberrio56@gmail.com>
2024-11-26 16:25:45 -0500

    app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,37 +9,50 @@ const routes: Routes = [
   },
   {
     path: 'admin-panel',
-    loadChildren: () => import('./pages/admin-panel/admin-panel.module').then( m => m.AdminPanelPageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    loadChildren: () =>
+      import('./pages/admin-panel/admin-panel.module').then(
+        (m) => m.AdminPanelPageModule
+      ),
   },
   {
     path: 'login-admin',
-    loadChildren: () => import('./pages/login-admin/login-admin.module').then( m => m.LoginAdminPageModule)
+    loadChildren: () =>
+      import('./pages/login-admin/login-admin.module').then(
+        (m) => m.LoginAdminPageModule
+      ),
   },
   {
     path: 'ofertas',
-    loadChildren: () => import('./pages/ofertas/ofertas.module').then( m => m.OfertasPageModule)
+    loadChildren: () =>
+      import('./pages/ofertas/ofertas.module').then((m) => m.OfertasPageModule),
   },
   {
     path: 'detalle-oferts/:id',
-    loadChildren: () => import('./pages/detalle-oferts/detalle-oferts.module').then(m => m.DetalleOfertsPageModule)
+    loadChildren: () =>
+      import('./pages/detalle-oferts/detalle-oferts.module').then(
+        (m) => m.DetalleOfertsPageModule
+      ),
   },
-  
+
   {
     path: 'auth-compani',
-    loadChildren: () => import('./pages/auth-compani/auth-compani.module').then( m => m.AuthCompaniPageModule)
+    loadChildren: () =>
+      import('./pages/auth-compani/auth-compani.module').then(
+        (m) => m.AuthCompaniPageModule
+      ),
   },
   {
     path: 'new-ofers',
-    loadChildren: () => import('./pages/new-ofers/new-ofers.module').then( m => m.NewOfersPageModule)
+    loadChildren: () =>
+      import('./pages/new-ofers/new-ofers.module').then(
+        (m) => m.NewOfersPageModule
+      ),
   },
-
-
-
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
